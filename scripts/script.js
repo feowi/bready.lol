1d4b14b9fce2cdbc0d456ee183cc931495b0bab1
toast <levirooth29@gmail.com>
2025-04-09 21:15:37 +0200

    not fully done but functioning..

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const userID = "738748102311280681";
+const userID = "428509980996534283";
 const elements = {
 	statusBox: document.querySelector(".status"),
 	statusImage: document.getElementById("status-image"),
